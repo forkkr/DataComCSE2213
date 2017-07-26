c0cad66cb3f4aa15b7bc72820b14be13e3419cd9
Kashob Roy <kashobroy@gmail.com>
2017-07-27 01:02:37 +0600

    Add files via upload

--- Server.java
+++ Server.java
@@ -1,82 +1,80 @@
-package datatelecom;
-
-import java.io.BufferedReader;
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.math.BigInteger;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-class Server {
-
-    static ServerSocket server;
-    static Socket client;
-    static PrintWriter dout;
-    static BufferedReader in, stdIn;
-    static DataInputStream is;
-
-   public static void main(String[] args) throws IOException
-   {
-       
+/**
+ * Created by kashob on 7/26/17.
+ */
+public class Server {
+    public static ServerSocket server;
+    public static Socket client;
+    public static DataInputStream input;
+    public static DataOutputStream output;
+    public static FileWriter write;
+    public static void main(String [] args) throws IOException {
         server = new ServerSocket(5555);
         client = server.accept();
-        System.out.println("client");
-        dout = new PrintWriter(client.getOutputStream());
-        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        is = new DataInputStream(client.getInputStream());
-        String msg;
-        while (true) {
-            msg = is.readUTF();
-            //System.out.println(msg +" meg");
-            if(msg=="stop")
-            {
+        input = new DataInputStream(client.getInputStream());
+        output = new DataOutputStream(client.getOutputStream());
+        write = new FileWriter("output.txt",true);
+        while(true)
+        {
+            String msg = input.readUTF();
+            System.out.println(msg +" just input");
+            if(msg.compareTo("stop")==0) {
+                write.flush();
+                write.close();
                 break;
-                
             }
-            if (paritChecking(msg)) {
-                System.out.print(getCharacter(msg));
+            if(isOkay(msg))
+            {
+                System.out.println(msg + "after checking");
+                output.writeUTF("Received");
+                output.flush();
+                System.out.println(correctOuput(msg) +"correct mag");
+                write.write(correctOuput(msg)+"\n");
+
             }
             else
             {
-            System.out.println("Error Found !!!");
-                
+                output.writeUTF("error");
+                output.flush();
             }
         }
-        
     }
-
-   
-
-    static boolean paritChecking(String msg) {
-
-        int num = Integer.parseInt(msg);
-        //System.out.println(num);
-        int count = function(num);
-        if (count % 2 == 1) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public static int function(int checkedNumber) {
-        BigInteger val = new BigInteger(String.valueOf(checkedNumber));
-        val = val.abs();
-        int count = val.bitCount();
-        String binaryString = val.toString(2);
-
-        //System.out.println("count = " + count);
-        //System.out.println("bin = " + binaryString);
-        return count;
-    }
-    static String getCharacter(String msg)
+    static boolean isOkay(String msg)
     {
-        int num = Integer.parseInt(msg);
-        num = num >> 1;
-        char ch = (char)num;
-        String ret = Character.toString(ch);
-        return ret;
+        String ar[] = msg.split("#");
+        int sum = 0;
+        for(int j =0; j < ar[0].length(); j++)
+        {
+            sum += ar[0].charAt(j);
+        }
+        for(int i = 1; i < ar.length - 1; i++ )
+        {
+            sum +='#';
+            for(int j =0; j < ar[i].length(); j++)
+            {
+                sum += ar[i].charAt(j);
+            }
+        }
+        int checksum = Integer.parseInt(ar[ar.length-1]);
+        System.out.println(sum +" sum&checksum "+ checksum);
+        if( (sum % 16) == checksum)
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    static String correctOuput(String msg) {
+    String ar[] = msg.split("#");
+    String ret =ar[0];
+    for(int i = 1; i < ar.length - 1; i++)
+    {
+        ret+="#";
+        ret+=ar[i];
+    }
+    return ret;
     }
 }
